0769b8a3f448d49570a6f2562ef839146caff950
oleksandr_shainoga <oleksandr_shainoga@epam.com>
2017-10-27 17:56:53 +0300

    try connect to cassandra

--- Spark/src/main/java/test/main.java
+++ Spark/src/main/java/test/main.java
@@ -5,13 +5,23 @@ import com.datastax.driver.core.*;
 
 public class main {
     public static void main(String[] args) {
-        Cluster cluster = Cluster.builder().addContactPoint("cassandra-1").build();
-        Session session = cluster.connect("demo");
-        session.execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
-        ResultSet results = session.execute("SELECT * FROM users WHERE lastname='Jones'");
-        for (Row row : results) {
-            System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+        // 9160
+        try {
+            Cluster cluster = Cluster.builder().addContactPoint("172.20.0.2").withPort(9160).build();
+            Session session = cluster.connect("demo");
+        } catch (Exception e)
+        {
+            e.printStackTrace();
         }
-        cluster.close();
+        Cluster cluster1 = Cluster.builder().addContactPoint("172.20.0.2").build();
+        Session session = cluster1.connect("demo");
+//        Session session = cluster.connect("demo");
+//        session.execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+//        ResultSet results = session.execute("SELECT * FROM users WHERE lastname='Jones'");
+//        for (Row row : results) {
+//            System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+//        }
+//        cluster.close();
+
     }
 }
